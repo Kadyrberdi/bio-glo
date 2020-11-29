50e4164e2fda9bc8f837d8e8e11a4f4fe1524d3f
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-11-29 06:37:11 +0300

    feat: add base functions to calculator

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,14 +1,39 @@
 const calc = () => {
     const checkbox = document.querySelectorAll('[type=checkbox]');
-    console.log('checkbox: ', checkbox);
+    const grandParent = document.getElementById('collapseTwo');
+    const parent = grandParent.querySelector('.panel-body');
+    
     let onoffswitchOne = document.getElementById('myonoffswitch'),
-        onoffswitchTwo = document.getElementById('myonoffswitch-two');
+        onoffswitchTwo = document.getElementById('myonoffswitch-two'),
+        countSeptic;
         
     const oneChamber = 10000,
         TwoChamber = 15000;
 
-    onoffswitchOne = TwoChamber;
+    const hideSecondWell = () => {
+        for (let i = 3; i < parent.children.length - 1; i++) {
+            parent.children[i].style.display = 'none';
+        }
+    };
+    const showSecondWell = () => {
+        for (let i = 3; i < parent.children.length - 1; i++) {
+            parent.children[i].style.display = 'inline-block';
+        }
+    };
 
+    checkbox.forEach(elem => {
+        elem.addEventListener('click', () => {
+            if (onoffswitchOne.checked === true) {
+                countSeptic = TwoChamber;
+                console.log('countSeptic: ', countSeptic);
+                hideSecondWell();
+            } else {
+                countSeptic = oneChamber;
+                console.log('countSeptic: ', countSeptic);
+                showSecondWell();
+            }
+        });
+    });
 
 };
 
